5a7f1a56850b9f1d1dc28633f34325673000fe55
David <huhexu@hotmail.com>
2018-03-14 12:07:35 +0800

    dboperate on oo method

--- db/tMySql.go
+++ db/tMySql.go
@@ -13,12 +13,103 @@ const (
 	c_DataBase_schema    = "transfusion"
 )
 
+//数据库连接类错误提示信息
+const (
+	c_Msg_DBConnect_Err  = "错误,无法连接到指定数据库！"
+	c_Msg_DBInsert_Err   = "错误,插入数据库操作失败！"
+	c_Msg_DBDelete_Err   = "错误,册除数据操作失败！"
+	c_Msg_DBTruncate_Err = "错误,册除指定数据表内所有信息失败！"
+)
+
+//输液用应用对象
 type detector struct {
 	detector_id string
 	receiver_id string
 	disable     bool /*是否启用*/
 }
 
+type DBConn struct {
+	UserName    string
+	Password    string
+	Schema      string
+	Port        string
+	IpAddr      string
+	IsConnected bool
+	DbHandler   *sql.DB
+}
+
+//局部数据库操作对象
+//var g_DbHandler *sql.DB
+
+//连接数据库
+func (this *DBConn) ConnectDB() error {
+	//	连接用数据库信息
+	strDataSource := this.UserName + ":" + this.Password + "@tcp(" + this.IpAddr + ":" + this.Port + ")/"
+	strDataSource = strDataSource + this.Schema + "?charset=utf8"
+	db, err := sql.Open("mysql", strDataSource)
+	defer db.Close()
+	if err != nil {
+		fmt.Println(c_Msg_DBConnect_Err)
+		panic(err.Error())
+		return err
+	}
+	this.DbHandler = db
+	if this.DbHandler.Stats().OpenConnections > 0 {
+		this.IsConnected = true
+	} else {
+		this.IsConnected = false
+	}
+	return nil
+}
+
+//插入数据到指定数据库内
+func (this *DBConn) InsertData(strSql string, strValues []string) (affected_Num int, err error) {
+	var result sql.Result
+	//	如果没有连接数据库则强制连接
+	if !this.IsConnected {
+		this.ConnectDB()
+	}
+	result, err = this.DbHandler.Exec(strsql, strValues)
+	if err != nil {
+		fmt.Println(c_Msg_DBInsert_Err)
+		return
+	}
+	affected_Num, _ = result.RowsAffected()
+	return affected_Num, err
+}
+
+//根据条件册除数据库
+func (this *DBConn) DeleteData(strSql string, strValues []string) (affected_Num int, err error) {
+	var result sql.Result
+	//	如果没有连接数据库则强制连接
+	if !this.IsConnected {
+		this.ConnectDB()
+	}
+	result, err = this.DbHandler.Exec(strsql, strValues)
+	if err != nil {
+		fmt.Println(c_Msg_DBDelete_Err)
+		return
+	}
+	affected_Num, _ := result.RowsAffected()
+	return affected_Num, err
+}
+
+//册除全表
+func (this *DBConn) TruncateTable(strTableName string) (affected_Num int, err error) {
+	result, err = this.DbHandler.Exec("Truncate Table " + strTableName)
+	if err != nil {
+		fmt.Println(c_Msg_DBTruncate_Err)
+		return
+	}
+	affected_Num, _ := result.RowsAffected()
+	return affected_Num, err
+}
+
+func (this *DBConn) QueryData(strSql string, args ...interface{}) (query_Results map[string]string, err error) {
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 func TestDb() {
 	db, err := sql.Open("mysql", "root:2341656@tcp(127.0.0.1:3306)/einfusion?charset=utf8")
 	if err != nil {
@@ -28,16 +119,25 @@ func TestDb() {
 	}
 	defer db.Close()
 	////////////////insert sample//////////////////////////////////////////////////////
-	var result sql.Result
-	result, err = db.Exec("insert into t_main(ip_address, unit_id,master_id,time) values(?,?,?,?)",
-		"127.0.0.2", "slave_002", "master_001", "2018-01-04")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	lastId, _ := result.LastInsertId()
-	fmt.Println("新插入记录的ID为", lastId)
+	//	var result sql.Result
+	//	result, err = db.Exec("insert into t_main(ip_address, unit_id,master_id,time) values(?,?,?,?)",
+	//		"127.0.0.2", "slave_002", "master_001", "2018-01-04")
+	//	if err != nil {
+	//		fmt.Println(err)
+	//		return
+	//	}
+	//	lastId, _ := result.LastInsertId()
+	//	fmt.Println("新插入记录的ID为", lastId)
 	/////////////////////////////////////////////////////////////////////////////////
+
+	//////////////////////////////insert sample by another way///////////////////////////////
+	//	stmt, err := db.Prepare(`insert into t_main(ip_address, unit_id,master_id,time) values(?,?,?,?)`)
+	//	checkErr(err)
+	//	res, err := stmt.Exec("127.0.0.2", "slave_003", "master_002", "2018-01-05")
+	//	checkErr(err)
+	//	lastId, _ = res.RowsAffected()
+	//	fmt.Println("新插入记录的ID为", lastId)
+	/////////////////////////////////////////////////////////////////////////////////////
 
 	/////////////////////select sample by one record/////////////////////////////////////
 	var row *sql.Row
@@ -65,15 +165,6 @@ func TestDb() {
 	}
 	rows.Close()
 	///////////////////////////////////////////////////////////////////////////////////////
-
-	//////////////////////////////insert sample by another way///////////////////////////////
-	stmt, err := db.Prepare(`insert into t_main(ip_address, unit_id,master_id,time) values(?,?,?,?)`)
-	checkErr(err)
-	res, err := stmt.Exec("127.0.0.2", "slave_003", "master_002", "2018-01-05")
-	checkErr(err)
-	lastId, _ = res.RowsAffected()
-	fmt.Println("新插入记录的ID为", lastId)
-	/////////////////////////////////////////////////////////////////////////////////////
 
 	//////////////////////////////////delete sample/////////////////////////////////////
 	//	db.Exec("truncate table t_test")
