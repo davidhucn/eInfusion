91def6d0edd9b3d44e4f137224ee963680e24a66
davidhucn <huhexu@hotmail.com>
2018-06-29 21:56:37 +0800

    tcp

--- tcpOperate/tTcpMain.go
+++ tcpOperate/tTcpMain.go
@@ -6,7 +6,7 @@ package tcpOperate
 import (
 	"eInfusion/comm"
 	"eInfusion/logs"
-	//	ep "eInfusion/protocol"
+	ep "eInfusion/protocol"
 	"net"
 	"strconv"
 	"strings"
@@ -32,7 +32,7 @@ func initClisConnMap() {
 }
 
 // 连接断开
-func connectionLost(conn *net.TCPConn) {
+func lostConn(conn *net.TCPConn) {
 	//连接断开这个函数被调用
 	addr := conn.RemoteAddr().String()
 	ip := strings.Split(addr, ":")[0]
@@ -63,20 +63,6 @@ func ckError(err error, title string, exit bool) {
 	}
 }
 
-//echo server Goroutine
-func EchoFunc(c EndPointer) {
-	//	FIXME: 修改这里
-	defer c.Conn.Close()
-	buf := make([]byte, 1024)
-	for {
-		_, err := c.Conn.Read(buf)
-		if err != nil {
-			//println("Error reading:", err.Error())
-			return
-		}
-	}
-}
-
 //   定时处理&延时处理
 func loopingCall(conn *net.TCPConn) {
 	pingTicker := time.NewTicker(30 * time.Second) // 定时
@@ -99,18 +85,43 @@ func loopingCall(conn *net.TCPConn) {
 }
 
 //连接初始处理(ed)
-func connectionMade(conn *net.TCPConn) {
+func initConn(conn *net.TCPConn) {
 	//初始化连接这个函数被调用
-
-	// ****建立conn映射
 	addr := conn.RemoteAddr().String()
 	ip := strings.Split(addr, ":")[0]
 	mkClisConn(ip, conn)
 
-	doLog("connectionMade:", addr)
+	//	doLog("connectionMade:", addr)
 
 	// ****定时处理(心跳等)
 	//	go loopingCall(conn)
+}
+
+//echo server Goroutine
+func receiveData(c *net.TCPConn) {
+	defer c.Conn.Close()
+	for {
+		//	指定接收数据包头的帧长
+		recDataHeader := make([]byte, ep.G_TsCmd.HeaderLength)
+		_, err := c.Conn.Read(recDataHeader)
+		if err != nil {
+			break
+		}
+		// 数据包数据内容长度记录变量
+		var intPckContentLength int
+		// 判断包头是否正确，如果正确，获取长度
+		if !ep.DecodeHeader(recDataHeader, &intPckContentLength) {
+			comm.Msg("调试信息：数据包头不正确")
+			continue
+		}
+		// 如果包头接收正确
+		r := make([]byte, intPckContentLength)
+		_, err = c.Conn.Read(r)
+		if !comm.CkErr("接收报文出错", err) {
+			// 处理报文数据内容
+			ep.DecodeRcvData(r, c.IPAddr)
+		}
+	}
 }
 
 //initial listener and run
@@ -125,16 +136,16 @@ func StartTcpServer(port int) {
 	comm.SepLi(60)
 	comm.Msg("TCP Server Info:" + tcpAddr.IP.String() + host)
 
-	conStream := make(chan *net.TCPConn)
+	connStream := make(chan *net.TCPConn)
 	initClisConnMap()
 	//打开N个Goroutine等待连接，Epoll模式
 	for i := 0; i < c_MaxConnectionAmount; i++ {
 		go func() {
-			for cs := range conStream {
-				connectionMade(cs)
-				//				EchoFunc(cs)
+			for cs := range connStream {
+				initConn(cs)
 				//TODO:【优先】接收数据
-				connectionLost(cs)
+
+				lostConn(cs)
 			}
 		}()
 	}
@@ -145,6 +156,11 @@ func StartTcpServer(port int) {
 			logs.LogMain.Error(c_Msg_Err_AcceptConnection)
 			continue
 		}
-		conStream <- lc
+		connStream <- lc
 	}
+}
+
+//   设置读数据超时
+func setReadTimeout(conn *net.TCPConn, t time.Duration) {
+	conn.SetReadDeadline(time.Now().Add(t))
 }
