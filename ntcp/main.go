726b5abffdd0d322d92b9aaa8f83882f416a2c0b
davidhucn <huhexu@hotmail.com>
2018-12-17 19:06:40 +0800

    1217

--- ntcp/main.go
+++ ntcp/main.go
@@ -1,7 +1,9 @@
 package ntcp
 
 import (
+	"bytes"
 	cm "eInfusion/comm"
+	"errors"
 	"log"
 	"net"
 	"sync"
@@ -19,7 +21,7 @@ type TServer struct {
 	address                  string // Address to open connection: localhost:9999
 	onNewClientCallback      func(c *Client)
 	onClientConnectionClosed func(c *Client, err error)
-	onNewDataReceive         func(c *Client, p []byte)
+	onNewDataReceived        func(c *Client, p []byte)
 	clients                  sync.Map //[string]*Client
 	timeOutDuration          time.Duration
 	packetHeader             *THeader
@@ -29,27 +31,37 @@ type TServer struct {
 func (c *Client) listen() {
 	connID := cm.GetRandString(8)
 	c.server.onNewClientCallback(c)
-	// 添加到服务器客户端映射中client id 是string
-	c.server.clients.Store(connID, c)
+	c.server.clients.Store(connID, c) // 添加到服务器客户端映射中client id 是string
 	defer c.conn.Close()
+	// 数据头包总长度
+	headerLength := c.server.packetHeader.length
+	headerBuffer := make([]byte, headerLength)
 	for {
-
+		_, err := c.conn.Read(headerBuffer)
 		if err != nil {
 			c.conn.Close()
 			c.server.clients.Delete(connID)
 			c.server.onClientConnectionClosed(c, err)
 			return
 		}
-		c.server.onNewDataReceive(c, p)
+		// 判断头数据是否正确
+		hCntStCursor := c.server.packetHeader.contentStartCursor // 开始下标
+		hCntLength := len(c.server.packetHeader.content)
+		cntBuffer := headerBuffer[hCntStCursor:hCntLength]
+		if !bytes.Equal(cntBuffer, headerBuffer) {
+			// 接收头数据包内数据不符合规定，则下线
+			c.conn.Close()
+			c.server.clients.Delete(connID)
+			c.server.onClientConnectionClosed(c, errors.New(TCPMsg.HeaderDataError))
+			return
+		}
+		// 开始接收内容
+		pLenCursor := c.server.packetHeader.packetLengthCursor
+		packetLength := cm.ConvertBytesToInt(headerBuffer[pLenCursor : pLenCursor+1])
+		p := make([]byte, packetLength)
+		c.conn.Read(p)
+		c.server.onNewDataReceived(c, p)
 	}
-}
-
-// VerifyPacketHeader :判断包头是否符合要求
-func (c *Client) verifyPacketHeader() bool {
-	length := c.server.packetHeader.length
-	headerBuffer := make([]byte, length)
-	c.conn.Read(headerBuffer)
-	for b:=range 
 }
 
 // Send text message to client
@@ -58,36 +70,38 @@ func (c *Client) Send(message string) error {
 	return err
 }
 
-// Send bytes to client
+// SendBytes :发送bytes到客户端
 func (c *Client) SendBytes(b []byte) error {
 	_, err := c.conn.Write(b)
 	return err
 }
 
-func (c *Client) Conn() net.Conn {
+// GetConnObject :获取conn对象
+func (c *Client) GetConnObject() net.Conn {
 	return c.conn
 }
 
+// Close ：关闭对象
 func (c *Client) Close() error {
 	return c.conn.Close()
 }
 
-// Called right after tcpserver starts listening new client
-func (s *TServer) onNewClient(callback func(c *Client)) {
+// WhenNewClientConnected :当有新连接时
+func (s *TServer) WhenNewClientConnected(callback func(c *Client)) {
 	s.onNewClientCallback = callback
 }
 
-// Called right after connection closed
-func (s *TServer) OnClientConnectionClosed(callback func(c *Client, err error)) {
+// WhenClientConnectionClosed :当连接下线时
+func (s *TServer) WhenClientConnectionClosed(callback func(c *Client, err error)) {
 	s.onClientConnectionClosed = callback
 }
 
-// Called when Client receives new message
-func (s *TServer) onNewDataReceive(callback func(c *Client, p []byte)) {
-	s.onNewDataReceive = callback
+// WhenNewDataReceived :接收到新数据包
+func (s *TServer) WhenNewDataReceived(callback func(c *Client, p []byte)) {
+	s.onNewDataReceived = callback
 }
 
-// Start network tcpserver
+// Listen :开始监听tcp服务
 func (s *TServer) Listen() {
 	listener, err := net.Listen("tcp", s.address)
 	if err != nil {
@@ -114,10 +128,9 @@ func NewTCPServer(address string, timeOutDuration time.Duration, packetHeader *T
 		timeOutDuration: timeOutDuration,
 		packetHeader:    packetHeader,
 	}
-
-	s.onNewClient(func(c *Client) {})
-	s.onNewDataReceive(func(c *Client, p []byte) {})
-	s.OnClientConnectionClosed(func(c *Client, err error) {})
+	s.WhenNewClientConnected(func(c *Client) {})
+	s.WhenNewDataReceived(func(c *Client, p []byte) {})
+	s.WhenClientConnectionClosed(func(c *Client, err error) {})
 
 	return s
 }
