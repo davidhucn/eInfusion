1d263332aadd3db19e2ac8964bb770802fe26b8c
davidhucn <huhexu@hotmail.com>
2018-12-18 21:59:15 +0800

    1218

---
+++ ntcp/tcp.go
@@ -0,0 +1,142 @@
+package ntcp
+
+import (
+	"bytes"
+	cm "eInfusion/comm"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+// Client holds info about connection
+type Client struct {
+	conn   net.Conn
+	server *TServer
+}
+
+// TServer :tcpserver
+type TServer struct {
+	address                  string // Address to open connection: localhost:9999
+	onNewClientCallback      func(c *Client)
+	onClientConnectionClosed func(c *Client, err error)
+	onNewDataReceived        func(c *Client, p []byte)
+	clients                  sync.Map //[string]*Client
+	timeOutDuration          time.Duration
+	packetHeader             *PacketHeader
+}
+
+// Read client data from channel
+func (c *Client) listen() {
+	connID := cm.GetRandString(10)
+	c.server.onNewClientCallback(c)
+	c.server.clients.Store(connID, c) // 添加到服务器客户端映射中client id 是string
+	defer c.conn.Close()
+	// 数据头包总长度
+	headerLength := c.server.packetHeader.length
+	headerBuffer := make([]byte, headerLength)
+	for {
+		_, err := c.conn.Read(headerBuffer)
+		if err != nil {
+			c.conn.Close()
+			c.server.clients.Delete(connID)
+			c.server.onClientConnectionClosed(c, err)
+			return
+		}
+		// 判断头数据是否正确
+		headerPrefixLength := len(c.server.packetHeader.prefixData)
+		if !bytes.Equal(headerBuffer[:headerPrefixLength], c.server.packetHeader.prefixData) {
+			// 接收头数据包内数据不符合规定，则下线
+			c.conn.Close()
+			c.server.clients.Delete(connID)
+			c.server.onClientConnectionClosed(c, errors.New(TCPMsg.HeaderDataError))
+			return
+		}
+		pLenCursor := c.server.packetHeader.packetLengthCursor // 数据包总长度
+		packetLength := cm.ConvertHexUnitToDecUnit(headerBuffer[pLenCursor])
+		if packetLength >= 128 { // 数据包长度限定
+			c.conn.Close()
+			c.server.clients.Delete(connID)
+			c.server.onClientConnectionClosed(c, errors.New(TCPMsg.ReceiveDataOutOfRange))
+			return
+		}
+		// 开始接收内容
+		packetDataLength := packetLength - uint8(headerLength)
+		dataBuffer := make([]byte, packetDataLength)
+		c.conn.Read(dataBuffer)
+		p := make([]byte, 0)
+		for _, h := range headerBuffer {
+			p = append(p, h)
+		}
+		for _, d := range dataBuffer {
+			p = append(p, d)
+		}
+		c.server.onNewDataReceived(c, p)
+	}
+}
+
+// SendBytes :发送bytes到客户端
+func (c *Client) SendBytes(b []byte) error {
+	_, err := c.conn.Write(b)
+	return err
+}
+
+// Boradcast :广播到所有客户端
+func (s *TServer) Boradcast(b []byte) error {
+	// s.clients.Range()
+	return nil
+}
+
+// WhenNewClientConnected :当有新连接时
+func (s *TServer) WhenNewClientConnected(callback func(c *Client)) {
+	s.onNewClientCallback = callback
+}
+
+// WhenClientConnectionClosed :当连接下线时
+func (s *TServer) WhenClientConnectionClosed(callback func(c *Client, err error)) {
+	s.onClientConnectionClosed = callback
+}
+
+// WhenNewDataReceived :接收到新数据包
+func (s *TServer) WhenNewDataReceived(callback func(c *Client, p []byte)) {
+	s.onNewDataReceived = callback
+}
+
+// Listen :开始监听tcp服务
+func (s *TServer) Listen() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", s.address)
+	if cm.CkErr(TCPMsg.SourceError, err) {
+		panic(err)
+	}
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if cm.CkErr(TCPMsg.SourceError, err) {
+		panic(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, _ := listener.Accept()
+		client := &Client{
+			conn:   conn,
+			server: s,
+		}
+		client.conn.SetReadDeadline(time.Now().Add(s.timeOutDuration))
+		go client.listen()
+	}
+}
+
+// NewTCPServer :Creates new tcp tcpserver instance
+func NewTCPServer(address string, timeOutDuration time.Duration, packetHeader *PacketHeader) *TServer {
+	fmt.Println(TCPMsg.StartServiceMsg, address)
+	s := &TServer{
+		address:         address,
+		timeOutDuration: timeOutDuration,
+		packetHeader:    packetHeader,
+	}
+	s.WhenNewClientConnected(func(c *Client) {})
+	s.WhenNewDataReceived(func(c *Client, p []byte) {})
+	s.WhenClientConnectionClosed(func(c *Client, err error) {})
+
+	return s
+}
--- ntcp/tcpmsg.go
+++ ntcp/tcpmsg.go
@@ -3,6 +3,7 @@ package ntcp
 type tcpMsg struct {
 	SendError              string
 	ReceiveError           string
+	ReceiveDataOutOfRange  string
 	StartServiceMsg        string
 	HeaderDataError        string
 	CanNotFindConnection   string
@@ -11,6 +12,9 @@ type tcpMsg struct {
 	SourceError            string
 	SendFailureForLongTime string
 }
+
+// TCPMsg :消息对象
+var TCPMsg tcpMsg
 
 func init() {
 	TCPMsg.SendError = "错误，发送数据错误！"
@@ -21,8 +25,6 @@ func init() {
 	TCPMsg.SourceError = "错误，TCP服务资源错误！"
 	TCPMsg.SendSuccess = "提示,发送指令和数据成功！"
 	TCPMsg.SendFailureForLongTime = "错误，由于设备长时间断线或者故障，发送指令和设备失败！"
+	TCPMsg.ReceiveDataOutOfRange = "接收的数据包超出限定长度"
 	TCPMsg.StartServiceMsg = "提示，TCP平台开始运行 ……"
 }
-
-// TCPMsg :消息对象
-var TCPMsg tcpMsg
