ba3294728de947062eadc56d46aac1b4a53f0dc5
davidhucn <huhexu@hotmail.com>
2018-12-26 23:08:06 +0800

    1216

--- ndb/comm.go
+++ ndb/comm.go
@@ -15,4 +15,21 @@ func init() {
 	DBType.Sqlite3 = "sqlite3"
 	DBType.Oracle = "oracle"
 	DBType.MSSql = "mssql"
+
+	DBMsg.ConnectDBErr = "错误,无法连接到指定数据库！"
+	DBMsg.InsertDataErr = "错误,插入数据库操作失败！"
+	DBMsg.DeleteDataErr = "错误,册除数据操作失败！"
+	DBMsg.QueryDataErr = "错误,查询数据信息失败！"
+	DBMsg.UpdateDataErr = "错误,更数据信息失败！"
 }
+
+type dbMsg struct {
+	ConnectDBErr  string
+	InsertDataErr string
+	DeleteDataErr string
+	QueryDataErr  string
+	UpdateDataErr string
+}
+
+// DBMsg :数据库消息对象
+var DBMsg dbMsg
--- ndb/main.go
+++ ndb/main.go
@@ -1,7 +1,11 @@
 package ndb
 
 import (
+	"eInfusion/comm"
+	"eInfusion/tlogs"
+
 	"github.com/jmoiron/sqlx"
+
 	// mysql 接口
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,31 +14,38 @@ import (
 
 // 数据库连接参数
 type dbParam struct {
-	UserName string
-	Pwd      string
-	Host     string
-	Port     string
+	UserName         string
+	Password         string
+	HostNameOrIPAddr string
+	Port             string
+	schemaName       string
 }
 
 // DBx :数据库对象
 type DBx struct {
-	db     *sqlx.DB
-	param  dbParam
-	dbType databaseTypeForDriver
+	db           *sqlx.DB
+	params       dbParam
+	databaseType string
 }
 
 // NewDBx :新建数据库对象
-func NewDBx(param dbParam, databaseType databaseTypeForDriver) *DBx {
+func NewDBx(param dbParam, dbType string) *DBx {
 	return &DBx{
-		param:  param,
-		dbType: databaseType,
+		params:       param,
+		databaseType: dbType,
 	}
 }
 
 // Connect :连接到数据库
 func (d *DBx) Connect() {
 	var err error
-	dbsource := ""
+	// "user:password@tcp(127.0.0.1:3306)/hello")
+	dbsource := d.params.UserName + ":" + d.params.Password + "@tcp(" + d.params.HostNameOrIPAddr + "):"
+	dbsource = dbsource + d.params.Port + "/" + d.params.schemaName
 
-	d.db, err = sqlx.Open(d.dbType, dbsource)
+	d.db, err = sqlx.Open(d.databaseType, dbsource)
+	if !comm.CkErr(DBMsg.ConnectDBErr, tlogs.Error, err) {
+
+	}
+
 }
